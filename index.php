e2879a88b18d5f5a1adb3d7bae481ee9705985d8
muharyaanandas <arnanda779@gmail.com>
2024-10-06 12:54:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard | SIAD Kalebajeng</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <style type="text/css">
+        .navbar-siad {
+            background: #49108B;
+            border-radius: 0px;
+            border: 1px solid #49108B;
+            margin: 0px;
+            padding: 20px 0px;
+        }
+        .navbar-inverse .navbar-brand {
+            color: #fff;
+        }
+        .navbar-inverse .navbar-nav>li>a {
+            color: #fff;
+        }
+        .navbar-siad>li>a {
+            color: #9d9d9d !important;
+        }
+        .banner {
+            background: #49108B;
+            border-radius: 0px;
+            border: 1px solid #49108B;
+            padding: 60px 0px;
+        }
+        .banner {
+            color: white;
+        }
+        .banner a {
+            padding: 15px 25px;
+            color: white;
+            border: 1px solid white;
+            -webkit-transition: all 0.5s;
+            transition: all 0.5s;
+            margin-right: 10px;
+        }
+        .banner a:hover {
+            text-decoration: none;
+            border: 1px dashed white;
+        }
+        .banner p {
+            font-size: 13pt;
+        }
+    </style>
+
+    <nav class="navbar navbar-inverse navbar-siad">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><b>ARSIP DIGITAL KALEBAJENG</b></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1"></div>
+        </div>
+    </nav>
+
+    <div class="banner">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div style="margin-top: 140px;">
+                        <h2>Selamat Datang di</h2>
+                        <h1>Sistem Informasi Arsip Digital</h1>
+                        <p>Manajemen file arsip dengan mudah dan cepat.</p>
+                        <br>
+                        <br>
+                        <a href="user_login.php">LOGIN USER</a>
+                        <a href="login.php">LOGIN ADMIN / PETUGAS</a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <img src="gambar/depan/8.png">
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
